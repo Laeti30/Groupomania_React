25f06b3f22e2f779d10d4df0b0641bc1b241c5e6
Laetitia Forgeur <l.forgeur@gmail.com>
2021-10-08 09:50:06 +0200

    Création page login/signup en statique

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import logo from '../images/icon-left-font-cut.jpg'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <>
       <section className='container'>
-        <img src={logo} alt='logo' className='logo' />
+        <Link to='/'>
+          <img src={logo} alt='logo' className='logo' />
+        </Link>
         <article className='form'>
           <form>
             <h2>Connexion</h2>
@@ -21,6 +24,12 @@ const Login = () => {
               Se connecter
             </button>
           </form>
+          <p>
+            Vous n'êtes pas encore inscrit ?
+            <Link to='/signup'>
+              <span className='signupLink'>Créer un compte</span>
+            </Link>
+          </p>
         </article>
       </section>
     </>
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import logo from '../images/icon-left-font-cut.jpg'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div>
-      <h1>Signup</h1>
-    </div>
+    <>
+      <section className='container'>
+        <Link to='/'>
+          <img src={logo} alt='logo' className='logo' />
+        </Link>
+        <article className='form'>
+          <form>
+            <h2>Inscription</h2>
+            <div className='form-control'>
+              <label htmlFor='lastName'>Nom de famille :</label>
+              <input type='text' id='lastName' name='lastName' />
+            </div>
+            <div className='form-control'>
+              <label htmlFor='firstName'>Prénom :</label>
+              <input type='text' id='firstName' name='firstName' />
+            </div>
+            <div className='form-control'>
+              <label htmlFor='email'>Email :</label>
+              <input type='email' id='email' name='email' />
+            </div>
+            <div className='form-control'>
+              <label htmlFor='password'>Mot de passe :</label>
+              <input type='password' id='password' name='password' />
+            </div>
+            <button type='submit' className='btn'>
+              S'inscrire
+            </button>
+          </form>
+          <p>
+            Vous avez déjà un compte ?
+            <Link to='/login'>
+              <span className='signupLink'>Se connecter</span>
+            </Link>
+          </p>
+        </article>
+      </section>
+    </>
   )
 }
 
